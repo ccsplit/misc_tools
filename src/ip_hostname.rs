55f2a50a8c09b0dc4101c52910270ad427f71e7d
ccsplit <cshay237@gmail.com>
2019-11-27 09:31:41 -0600

    Finished ip_hostname.
    Finished adding all of the functionality to ip_hostname. It should
    now properly handle IpRanges, IpAddr, and also CIDRs. It also has an
    option to write the output to a file.

--- src/ip_hostname.rs
+++ src/ip_hostname.rs
@@ -1,37 +1,50 @@
+//
+// ip_hostname.rs
+// Copyright (C) 2019 ccsplit <cshay237@gmail.com>
+// Distributed under terms of the MIT license.
+//
 extern crate clap;
+extern crate dns_lookup;
 #[macro_use]
 extern crate log;
+extern crate ipnet;
 extern crate simplelog;
+extern crate threadpool;
 
 use clap::{App, Arg};
+
+use dns_lookup::lookup_addr;
+
+use ipnet::{IpAddrRange, IpNet, Ipv4AddrRange, Ipv6AddrRange};
+
 use simplelog::*;
+
+use std::error::Error;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Result};
+use std::io::{BufRead, BufReader, Result, Write};
 use std::net::IpAddr;
 use std::path::Path;
 use std::process;
 use std::str::FromStr;
-use trust_dns_resolver::config::{ResolverConfig, ResolverOpts};
-use trust_dns_resolver::Resolver;
+use std::string::String;
+use std::sync::mpsc::channel;
+use std::vec::Vec;
+
+use threadpool::ThreadPool;
 
 fn main() {
-    let matches = App::new("IP => Hostnames")
+    let matches = App::new("Ip => Hostname")
         .version("0.1.0")
         .author("ccsplit")
-        .about("Takes a file of IPs, CIDRs and ranges to try and get a list of hostnames.")
-        .arg(
-            Arg::with_name("IPFILE")
-                .help("The file with IP/CIDRs/Ranges which you need to check.")
-                .required(true)
-                .index(1),
+        .about(
+            "A utility to grab the hostnames for the specified IPs/CIDRs/Ranges
+            using reverse DNS lookups.",
         )
         .arg(
-            Arg::with_name("output")
-                .short("o")
-                .long("output")
-                .value_name("OUTPUT")
-                .help("Place the valid hostnames within the given filename.")
-                .takes_value(true),
+            Arg::with_name("IPS")
+                .help("An IP, CIDR, IPRange, or file if the (-f|--file) flag is specified.")
+                .required(true)
+                .index(1),
         )
         .arg(
             Arg::with_name("v")
@@ -39,28 +52,145 @@ fn main() {
                 .multiple(true)
                 .help("Sets the level of verbosity"),
         )
+        .arg(
+            Arg::with_name("file")
+                .short("f")
+                .help("The passed in value is a file."),
+        )
+        .arg(
+            Arg::with_name("threads")
+                .short("t")
+                .long("threads")
+                .value_name("THREADS")
+                .help("The number of threads to use when checking the URLs.")
+                .takes_value(true)
+                .default_value("10"),
+        )
+        .arg(
+            Arg::with_name("output")
+                .short("o")
+                .long("output")
+                .value_name("OUTPUT")
+                .help("Place the valid IPs and hostnames within the given filename.")
+                .takes_value(true),
+        )
         .get_matches();
-    // Check if the URLFILE exists if not exit with error message.
-    let ipfile = matches.value_of("IPFILE").unwrap();
     let verbosity = matches.occurrences_of("v");
     create_logger(verbosity);
-    if !Path::new(ipfile).exists() {
-        error!("IPFile: '{}' does not exist!", ipfile);
-        process::exit(1)
+    let mut ips = Vec::new();
+    let write_file = matches.is_present("output");
+    let ips_value = matches.value_of("IPS").unwrap();
+    let num_workers = clap::value_t!(matches.value_of("threads"), usize).unwrap_or(10);
+    let is_file = matches.is_present("file");
+    let pool = ThreadPool::with_name("check_url worker".to_owned(), num_workers);
+    let (tx, rx) = channel();
+    if is_file {
+        if !Path::new(ips_value).exists() {
+            error!("IPS File: '{}' does not exist!", ips_value);
+            process::exit(1)
+        }
+        let file = File::open(ips_value).unwrap();
+        let lines = BufReader::new(file).lines();
+        for line in lines {
+            ips.push(line.unwrap());
+        }
+    } else {
+        ips.push(String::from(ips_value));
     }
-    let file = File::open(ipfile).unwrap();
-    let outfile = matches.value_of("output").unwrap_or("");
-    let mut resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
+    for ip_obj in &ips {
+        if let Some(_) = ip_obj.find("/") {
+            // Handle if the string is a CIDR..
+            let net = IpNet::from_str(ip_obj).unwrap();
+            trace!("Parsed: '{:?}", net);
+            for ip in net.hosts() {
+                let tx = tx.clone();
+                pool.execute(move || {
+                    trace!("Creating thread for: '{}'", ip);
+                    if let Some(i) = resolve_ip(ip) {
+                        let msg = format!("{} => {}", ip, i);
+                        info!("{}", msg);
+                        if write_file {
+                            tx.send(msg);
+                        }
+                    }
+                });
+            }
+        } else if let Some(_) = ip_obj.find("-") {
+            // Handle if the string is an IPRange.
+            let ips: Vec<&str> = ip_obj.split("-").collect();
+            let ip_range: IpAddrRange;
+            if let Some(_) = ips[0].find(':') {
+                // IPv6
+                ip_range = IpAddrRange::from(Ipv6AddrRange::new(
+                    ips[0].parse().unwrap(),
+                    ips[1].parse().unwrap(),
+                ));
+            } else {
+                ip_range = IpAddrRange::from(Ipv4AddrRange::new(
+                    ips[0].parse().unwrap(),
+                    ips[1].parse().unwrap(),
+                ));
+            }
+            for ip in ip_range {
+                let tx = tx.clone();
+                pool.execute(move || {
+                    trace!("Creating thread for: '{}'", ip);
+                    if let Some(i) = resolve_ip(ip) {
+                        let msg = format!("{} => {}", ip, i);
+                        info!("{}", msg);
+                        if write_file {
+                            tx.send(msg);
+                        }
+                    }
+                });
+            }
+        } else {
+            let ip = ip_obj.parse::<IpAddr>().unwrap();
+            let tx = tx.clone();
+            pool.execute(move || {
+                trace!("Creating thread for: '{}'", ip);
+                if let Some(i) = resolve_ip(ip) {
+                    let msg = format!("{} => {}", ip, i);
+                    info!("{}", msg);
+                    if write_file {
+                        tx.send(msg);
+                    }
+                }
+            });
+        }
+        pool.join();
+    }
+    drop(tx);
+    if write_file {
+        let outfile = Path::new(matches.value_of("output").unwrap());
+        let display = outfile.display();
 
-    for line in BufReader::new(file).lines() {
-        // Test the url to see if it is valid and if so print to the screen/output file.
-        let value = line.unwrap();
-        trace!("Attempting to resolve hostname for IP: {}", value);
-        let response = resolver
-            .reverse_lookup(IpAddr::from_str(&value).unwrap())
-            .unwrap();
-        println!("{:?}", response);
+        let mut file = match File::create(&outfile) {
+            Err(why) => panic!("Unable to create {}: {}", display, why.description()),
+            Ok(file) => file,
+        };
+        println!("Writing the results to: {}", display);
+        for valid in rx {
+            file.write_fmt(format_args!("{}\n", valid));
+        }
     }
+}
+
+fn resolve_ip(ip: IpAddr) -> Option<String> {
+    trace!("Attempting to resolve: '{}", ip);
+    match lookup_addr(&ip) {
+        Ok(r) => {
+            trace!("Resolved ip address '{}' to '{}", ip, r);
+            if format!("{}", ip) == format!("{}", r) {
+                return None;
+            }
+            return Some(r);
+        }
+        Err(err) => {
+            trace!("Unable to resolve: '{:?}'. Error occurred:\n{}", ip, err);
+            return None;
+        }
+    };
 }
 
 fn create_logger(level: u64) {
